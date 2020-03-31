8f016923359c7b1a5bd908315a2c417f936ff818
Barb1999 <61975391+Barb1999@users.noreply.github.com>
2020-03-31 21:18:04 +0200

    Update PrimeNumbers.java

--- src/main/java/sgh/PrimeNumbers.java
+++ src/main/java/sgh/PrimeNumbers.java
@@ -3,7 +3,24 @@ package sgh;
 public class PrimeNumbers {
 
     public static void primes(int n) {
-        // Your code here
+        int sum = 0; int a = 0; int b; int c = 1;
+        while (a < n) {
+            for (b = 1; b <= j; b++) {
+                if (c % b == 0) {
+                    sum = sum + 1;
+                }
+            }
+            if (sum == 2) {
+                a = a + 1;
+                if (a < n) {
+                    System.out.print(c + ", ");
+                } else {
+                    System.out.print(c + "\n");
+                }
+            }
+            c ++;
+            sum = 0;
+        }
         System.out.println("2, 3, 5, 7");
     }
 
