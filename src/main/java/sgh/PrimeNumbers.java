62e4727284a310b18991c9cd4f9688d5de340668
Barb1999 <61975391+Barb1999@users.noreply.github.com>
2020-03-31 21:26:18 +0200

    Update PrimeNumbers.java

--- src/main/java/sgh/PrimeNumbers.java
+++ src/main/java/sgh/PrimeNumbers.java
@@ -5,7 +5,7 @@ public class PrimeNumbers {
     public static void primes(int n) {
         int sum = 0; int a = 0; int b; int c = 1;
         while (a < n) {
-            for (b = 1; b <= j; b++) {
+            for (b = 1; b <= c; b++) {
                 if (c % b == 0) {
                     sum = sum + 1;
                 }
@@ -21,9 +21,8 @@ public class PrimeNumbers {
             c ++;
             sum = 0;
         }
-        System.out.println("2, 3, 5, 7");
+       
     }
-
 
     public static void main(String[] args) {
         primes(4);
